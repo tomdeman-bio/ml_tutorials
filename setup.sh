7393f9ecb79c5687fb4bd3580deac9e8bdd8ebd1
brettin <brettin@anl.gov>
2017-03-10 15:48:12 -0600

    adding conda commands

--- setup.sh
+++ setup.sh
@@ -4,3 +4,11 @@ wget https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh
 chmod u+x Anaconda2-4.3.1-Linux-x86_64.sh
 ./Anaconda2-4.3.1-Linux-x86_64.sh -b
 
+# Create the ML_Tutorial environment
+conda create –f ML_Tutorial.yml
+
+# Activate the environment
+source activate ML_Tutorial
+
+# Close the environment once done
+# source deactivate
